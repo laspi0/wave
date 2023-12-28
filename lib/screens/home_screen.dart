08d6bd4d3bd0759d44c887d117a5b8acc1868bbd
laspi <pro43071919@gmail.com>
2023-12-28 16:17:25 +0000

    ajustement de l'appBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,74 +13,87 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize:
-            const Size.fromHeight(65.0), // Hauteur personnalisée de l'AppBar
-        child: AppBar(
-          backgroundColor: primaryColor,
-          leading: IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.settings,
-              color: Colors.white,
-              size: 25,
-            ),
-          ),
-          title: Row(
-              // mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                isVisible
-                    ? RichText(
-                        text: const TextSpan(
-                            text: "5.000",
-                            style: TextStyle(
-                              fontSize: 25,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w900,
-                            ),
-                            children: [
-                            TextSpan(
-                                text: "F",
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+              backgroundColor: primaryColor,
+              pinned: true,
+              floating: true,
+              expandedHeight: 100,
+              leading: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.settings,
+                  color: Colors.white,
+                  size: 25,
+                ),
+              ),
+              flexibleSpace: FlexibleSpaceBar(
+                title: Container(
+                  // color: Colors.black,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 48.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment:
+                          CrossAxisAlignment.center, // Ajouter cette ligne
+                      children: [
+                        isVisible
+                            ? RichText(
+                                text: const TextSpan(
+                                    text: "0",
+                                    style: TextStyle(
+                                      fontSize: 25,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w900,
+                                    ),
+                                    children: [
+                                    TextSpan(
+                                        text: "F",
+                                        style: TextStyle(
+                                          fontSize: 18,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w600,
+                                        ))
+                                  ]))
+                            : const Text(
+                                "••••••••",
                                 style: TextStyle(
-                                  fontSize: 18,
+                                  fontSize: 35,
                                   color: Colors.white,
                                   fontWeight: FontWeight.w600,
-                                ))
-                          ]))
-                    : const Text(
-                        "••••••••",
-                        style: TextStyle(
-                          fontSize: 35,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              isVisible = !isVisible;
+                            });
+                          },
+                          child: isVisible
+                              ? const Icon(
+                                  Icons.visibility_off_rounded,
+                                  size: 16,
+                                  color: Colors.white,
+                                )
+                              : const Icon(
+                                  Icons.visibility_rounded,
+                                  size: 16,
+                                  color: Colors.white,
+                                ),
                         ),
-                      ),
-                RichText(
-                    text: const TextSpan(
-                  children: [],
-                )),
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      isVisible = !isVisible;
-                    });
-                  },
-                  icon: isVisible
-                      ? const Icon(
-                          Icons.visibility_off_rounded,
-                          size: 16,
-                          color: Colors.white,
-                        )
-                      : const Icon(
-                          Icons.visibility_rounded,
-                          size: 16,
-                          color: Colors.white,
-                        ),
-                )
-              ]),
-          centerTitle: true,
-        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )),
+          SliverToBoxAdapter(
+            child: Container(
+              height: 1000,
+              color: Colors.amber,
+            ),
+          )
+        ],
       ),
     );
   }
