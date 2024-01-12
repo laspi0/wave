35f9eb14efb419568f7f501ff4817f952459f86c
laspi <pro43071919@gmail.com>
2024-01-12 05:33:41 +0000

    la page de settings

--- lib/screens/settingScreen.dart
+++ lib/screens/settingScreen.dart
@@ -10,6 +10,288 @@ class SettingScreen extends StatefulWidget {
 class _SettingScreenState extends State<SettingScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 243, 244, 246),
+        appBar: AppBar(
+          title: const Text(
+            'Paramètres',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: const Color.fromARGB(255, 243, 244, 246),
+        ),
+        body: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 20.0, top: 20),
+              child: Row(
+                children: [
+                  Text(
+                    'Partager',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 141, 141, 141)),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(
+                  horizontal: 8.0), // Ajustez la valeur selon vos besoins
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+              ),
+              height: 120,
+              width: 500,
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Icon(
+                        Icons.share,
+                        size: 35,
+                      ),
+                      Text(
+                        'Inviter un ami à rejoindre Wave',
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(
+                        width: 100,
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Icon(
+                        Icons.auto_awesome,
+                        size: 35,
+                      ),
+                      Text(
+                        'Utiliser le code promotionnel',
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(
+                        width: 100,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 20.0, top: 20),
+              child: Row(
+                children: [
+                  Text(
+                    'Support',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 141, 141, 141)),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(
+                  horizontal: 8.0), // Ajustez la valeur selon vos besoins
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+              ),
+              height: 150,
+              width: 500,
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        size: 35,
+                      ),
+                      Text(
+                        'Contactez le service client',
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(
+                        width: 130,
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Icon(
+                        Icons.assignment,
+                        size: 35,
+                      ),
+                      Text(
+                        'Vérifiez votre plafond',
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(
+                        width: 160,
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Icon(
+                        Icons.place,
+                        size: 35,
+                      ),
+                      Text(
+                        'Trouvez les agents à promixité',
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(
+                        width: 100,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 20.0, top: 20),
+              child: Row(
+                children: [
+                  Text(
+                    'Sécurité',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 141, 141, 141)),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(
+                  horizontal: 8.0), // Ajustez la valeur selon vos besoins
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+              ),
+              height: 120,
+              width: 500,
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Icon(
+                        Icons.share,
+                        size: 35,
+                      ),
+                      Text(
+                        'Vos appareils connectés',
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(
+                        width: 130,
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Icon(
+                        Icons.shield,
+                        size: 35,
+                      ),
+                      Text(
+                        'Modifier votre code secret',
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(
+                        width: 110,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(
+                  horizontal: 8.0), // Ajustez la valeur selon vos besoins
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+              ),
+              height: 65,
+              width: 500,
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Icon(
+                        Icons.logout,
+                        size: 35,
+                      ),
+                      Text(
+                        'Se déconnecter (78 481 94 31)',
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(
+                        width: 80,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 100,
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'wave digital finance',
+                    style: TextStyle(fontSize: 12, color: Colors.grey),
+                  ),
+                  Text(
+                    'Version 23.12.12-bc4cf3',
+                    style: TextStyle(fontSize: 12, color: Colors.grey),
+                  ),
+                  Text(
+                    'Condition Générales | Avis de Confidentialité',
+                    style: TextStyle(fontSize: 12, color: Colors.grey),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,3 +4,5 @@ Color primaryColor = const Color(0xff4749cd);
 Color secondColor = const Color(0x004b42dd);
 Color secondColo = const Color(0x00dee5ff);
 Color secondCol = const Color(0x00fff8d3);
+Color secondCo2 = const Color(0x00f3f4f6);
+
